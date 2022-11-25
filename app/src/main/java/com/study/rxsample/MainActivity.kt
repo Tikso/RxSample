504cceb149c4a493151cd2354af3f5a39bd74755
tiksontom <tiksontom@gmail.com>
2022-11-25 13:23:01 +0530

    add gradle commit

--- app/src/main/java/com/study/rxsample/MainActivity.kt
+++ app/src/main/java/com/study/rxsample/MainActivity.kt
@@ -7,5 +7,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        simpleSyncList()
+    }
+// simple synchronous list
+    private fun simpleSyncList(){
+        var list = listOf(1, 2, 3)
+        for (number in list) {
+            println(number)
+            list = listOf(4, 5, 6)
+        }
+        print(list)
+
     }
 }
